8b46e5e6a114989b16d0dd8f197ddc0ba3c8b3fb
Hernandez <eduardo.a.hernandez@sogeti.com>
2021-03-11 21:28:09 +0100

    structure folder and mongoose configuration with mongoAtlas

--- dist/app.js
+++ dist/app.js
@@ -3,6 +3,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = require("dotenv");
+dotenv_1.config();
 const express_1 = __importDefault(require("express"));
+const mongoose_1 = __importDefault(require("mongoose"));
 const app = express_1.default();
-app.listen(5000);
+app.use(express_1.default.json());
+const port = process.env.PORT || 3000;
+mongoose_1.default.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+});
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
+});
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,22 @@
+import { config } from 'dotenv';
+config();
 import express from 'express';
+import mongoose from 'mongoose';
+
 
 const app = express();
+app.use(express.json());
 
-app.listen(5000);
+const port = process.env.PORT || 3000;
+
+mongoose.connect((process.env.MONGODB_URL as string), {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
+});
