dc678bb2760382f44921a4195a1795478475bb5c
Hernandez <eduardo.a.hernandez@sogeti.com>
2021-03-12 15:51:43 +0100

    CREATE, DELETE, PUT,DELETE user on signup

--- src/routers/masterRouters.ts
+++ src/routers/masterRouters.ts
@@ -1,15 +1,22 @@
 import { Router } from 'express';
 
 import { findAllLinks, findLinkById, postLink, updatLink, deleteLink } from '../controllers/link'
+import { findAllUsers, findUserById, creatUser, deleteUser } from '../controllers/user'
 
 
 const router = Router();
-
+// link
 router.get('/links', findAllLinks);
 router.get('/links/:id',  findLinkById);
 router.post('/links',  postLink);
 router.put('/links/:id', updatLink);
 router.delete('/links/:id', deleteLink);
+
+// user
+router.get('/users', findAllUsers);
+router.get('/users/:id',  findUserById);
+router.post('/users',  creatUser);
+router.delete('/users/:id', deleteUser);
 
 
 export default router;
